1dcb7cd31b86a2133d9c1a40cf593583ecfdc493
Doktatech <Doktatech2@gmail.com>
2019-06-23 13:41:25 +0300

    Create Image model for the app

--- instagramApp/models.py
+++ instagramApp/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import User
 from django.db import models
+import datetime as dt
+
+
 
 # Create your models here.
+class caption(models.Model):
+    name =models.CharField(max_length=30)
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
+    user_bio = models.TextField()
+    user = models.ForeignKey(User)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering =['-last_update']
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+    
+class Photo(models.Model):
+    pub_date = models.DateTimeField(auto_now_add=True)
+    photo_name =models.CharField(max_length=30, null=True)
+    photo = models.ImageField(upload_to='photos/',blank=True)
+    user =models.ForeignKey(User,on_delete=models.CASCADE)
+    caption = models.ManyToManyField(caption)
+    likes = models.IntegerField(default=0,null=True )
+    profile = models.ForeignKey(Profile, null=True)
+    
+    @classmethod
+    def photos_newest(cls):
+        today = dt.date.today()
+        photos = cls.objects.filter(pub_date__date=today)
+        return photos
+    @classmethod
+    def photo_today(cls,date):
+        photos = cls.objects.filter(pub_date__date=date)
+        return photos
+
+
+
+
+
+
+
+
+
+
+
