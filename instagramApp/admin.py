e36768b033b26aae70c43738022b11f22ecfebe4
Doktatech <Doktatech2@gmail.com>
2019-06-23 13:42:03 +0300

    Link models to Django admin

--- instagramApp/admin.py
+++ instagramApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Photo,caption
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal =('caption')
+
+    admin.site.register(Photo)
+    admin.site.register(caption)
