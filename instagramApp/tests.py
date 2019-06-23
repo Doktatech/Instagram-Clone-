06bcbd2eba21254848415f05fea98cff365377c1
Doktatech <Doktatech2@gmail.com>
2019-06-23 21:30:45 +0300

    Second database migrations

--- instagramApp/tests.py
+++ instagramApp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Photo,tags
 
 # Create your tests here.
+class PhotoTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.photo= Photo(pub_date='2019',photo='null.png',likes=0,favorites=0)
